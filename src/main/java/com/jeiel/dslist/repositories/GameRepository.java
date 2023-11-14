64aa5d1a0401869a9780d84028b52a655ab08bc0
Jeiel <jeiellucas0404@gmail.com>
2023-11-13 22:22:45 -0300

    Game repository, service, controller

---
+++ src/main/java/com/jeiel/dslist/repositories/GameRepository.java
@@ -0,0 +1,8 @@
+package com.jeiel.dslist.repositories;
+
+import com.jeiel.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
